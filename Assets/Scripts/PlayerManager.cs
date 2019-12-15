ed7ee9f2941fb565f761c65a416cb75c71fde164
Jonathan Fraimorice <yonifra@gmail.com>
2019-12-15 06:04:33 +0200

    added player, material and movement logic

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public float directionalForce = 1500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d"))
+        {
+            rigidbody.AddForce(directionalForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rigidbody.AddForce(-directionalForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rigidbody.AddForce(0, 0, directionalForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rigidbody.AddForce(0, 0, -directionalForce * Time.deltaTime);
+        }
+    }
+}
