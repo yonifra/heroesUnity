eebe6029a686baffd517e1833d10c22c094882be
Jonathan Fraimorice <yonifra@gmail.com>
2019-12-19 23:53:33 +0200

    player manager logic

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,28 +4,34 @@ public class PlayerManager : MonoBehaviour
 {
     public Rigidbody rigidbody;
     public float directionalForce = 1500f;
+    public float jumpingForce = 2500f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey("d"))
+        if (Input.GetKey(KeyCode.D))
         {
             rigidbody.AddForce(directionalForce * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetKey("a"))
+        if (Input.GetKey(KeyCode.A))
         {
             rigidbody.AddForce(-directionalForce * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetKey("w"))
+        if (Input.GetKey(KeyCode.W))
         {
             rigidbody.AddForce(0, 0, directionalForce * Time.deltaTime);
         }
 
-        if (Input.GetKey("s"))
+        if (Input.GetKey(KeyCode.S))
         {
             rigidbody.AddForce(0, 0, -directionalForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddForce(0, jumpingForce * Time.deltaTime, 0);
         }
     }
 }
